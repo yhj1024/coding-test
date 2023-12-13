f3245e4c72aa97817108b38ea5a4d2eaa031a6d0
yoohyojun <80876813+yoohyojun@users.noreply.github.com>
2023-12-13 11:12:31 +0900

    [Bronze V] Title: 문자열, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/9086. 문자열/문자열.py
+++ 백준/Bronze/9086. 문자열/문자열.py
@@ -1,8 +1,5 @@
 count = int(input())
 words = [""] * count
-for i in range(0, count):
-    words[i] = input()
-
-for word in words:
-    wordArr = list(word)
-    print(wordArr[0] + wordArr[len(wordArr)-1])
+for _ in range(count):
+    wordArr = list(input())
+    print(wordArr[0] + wordArr[-1])
