457dc9300ac341183070bee748aa7accd815edac
yoohyojun <80876813+yoohyojun@users.noreply.github.com>
2023-12-16 17:16:57 +0900

    [Bronze II] Title: 진법 변환, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.py
+++ 백준/Bronze/2745. 진법 변환/진법 변환.py
@@ -1,2 +1,17 @@
 n, b = input().split()
-print(int(n, int(b)))
+b = int(b)
+result = 0
+
+def getValue(v):
+    if v.isalpha():
+        return int(ord(v) - ord('A')) + 10
+    else:
+        return int(v)
+
+# n을 뒤집어서 반복문을 돌면서
+# 현재 진법 ** 인덱스 * 값을 누적시킨다
+for idx, i in enumerate(reversed(n)):
+    result += (b ** idx) * getValue(i)
+
+# 값을 출력한다.
+print(result)
