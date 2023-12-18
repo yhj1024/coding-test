35bd94308ca009a9ad8a851f7ed1716cb069fb51
yoohyojun <80876813+yoohyojun@users.noreply.github.com>
2023-12-18 18:09:15 +0900

    [Bronze II] Title: 소수, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2581. 소수/소수.py
+++ 백준/Bronze/2581. 소수/소수.py
@@ -7,7 +7,7 @@ L = []
 def is_prime(val):
     if val < 2:
         return False
-    for i in range(2, val):
+    for i in range(2, int(val ** 0.5) + 1):
         if val % i == 0:
             return False
     return True
