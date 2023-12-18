ba3c058f419e9fe65c35b9c7d43422a662bb13e2
yoohyojun <80876813+yoohyojun@users.noreply.github.com>
2023-12-18 15:00:35 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 84 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -1,39 +1,11 @@
-from collections import OrderedDict
+changes = [25, 10, 5, 1]
 
 T = int(input())
 
-COIN_TYPES = OrderedDict([
-    ('quarter', 25),
-    ('dime', 10),
-    ('nickel', 5),
-    ('penny', 1)
-])
-
-
-def calculate_exchange(total_change):
-    counts = {
-        'quarter_count': 0,
-        'dime_count': 0,
-        'nickel_count': 0,
-        'penny_count': 0
-    }
-    for coinName, coinValue in COIN_TYPES.items():
-        if total_change == 0:
-            break
-        else:
-            counts[coinName+'_count'] = total_change // coinValue
-            total_change %= coinValue
-
-    return counts
-
-
-def do_print(targets):
-    for key, value in targets.items():
-        print(value, end=' ')
-    print()
-
-
-for i in range(T):
+for _ in range(T):
     C = int(input())
-    result = calculate_exchange(C)
-    do_print(result)
+    for i in changes:
+        count = C // i
+        print(count, end=' ')
+        C %= i
+    print()
