0d52195fd68ae7ace50486a1205f22da83733526
yoohyojun <80876813+yoohyojun@users.noreply.github.com>
2023-12-19 12:44:33 +0900

    [Bronze III] Title: 네 번째 점, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/3009. 네 번째 점/네 번째 점.py
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.py
@@ -1,25 +1,17 @@
 xs = []
 ys = []
-result = []
+
 for _ in range(3):
     x, y = map(int, input().split())
     xs.append(x)
     ys.append(y)
-# 첫번째랑 두번째랑 똑같으면 세번째꺼 출력
-# 첫번째랑 세번째랑 똑같으면 두번째꺼 출력
-# 두번째랑 세번째랑 똑같으면 첫번째꺼 출력
-if xs[0] == xs[1]:
-    result.append(xs[2])
-elif xs[0] == xs[2]:
-    result.append(xs[1])
-else:
-    result.append(xs[0])
 
-if ys[0] == ys[1]:
-    result.append(ys[2])
-elif ys[0] == ys[2]:
-    result.append(ys[1])
-else:
-    result.append(ys[0])
+for i in range(3):
+    if xs.count(xs[i]) == 1:
+        print(xs[i], end=' ')
+        break
 
-print(*result)
+for i in range(3):
+    if ys.count(ys[i]) == 1:
+        print(ys[i])
+        break
