aa9a28f4e6f5734e750bcfc85558c53b528f1657
yoohyojun <80876813+yoohyojun@users.noreply.github.com>
2023-12-20 10:08:01 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.py
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.py
@@ -1,10 +1,7 @@
 # 1부터 입력 받은 값 -1 합하면 됨
 
 def add_calculate(value):
-    sum = 0
-    for i in range(1, value+1):
-        sum += i
-    return sum
+    return sum(i for i in range(1, value+1))
 
 print(add_calculate(int(input())-1))
 print(2)
