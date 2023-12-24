38646af550126b70be2d6fda877d658cc4c451a5
yoohyojun <80876813+yoohyojun@users.noreply.github.com>
2023-12-25 06:37:04 +0900

    [Silver III] Title: 분수 합, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1735. 분수 합/분수 합.py
+++ 백준/Silver/1735. 분수 합/분수 합.py
@@ -4,22 +4,18 @@ A1, B1 = map(int, sys.stdin.readline().split())
 A2, B2 = map(int, sys.stdin.readline().split())
 
 
-def gcd(A, B):
+def gcd(a, b):
     # 최대공약수 구하기
-    x = A
-    y = B
-    while True:
-        r = x % y
-        if r != 0:
-            x = y
-            y = r
-        else:
-            break
-    return y
+    while a % b != 0:
+        tmp = b
+        b = a % b
+        a = tmp
+    return b
+
 
 def lcm(A, B):
     # 최소공배수는 두수의 곱 / 최대공약수
-    return A * B * gcd(A, B)
+    return A * B / gcd(A, B)
 
 
 def solution():
@@ -31,5 +27,6 @@ def solution():
         print(int(A/k), int(B/k))
     else:
         print(int(A), int(B))
+
 
 solution()
