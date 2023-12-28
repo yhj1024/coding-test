cb72703bcf53a51b7c52ee5f57995a9d6022f140
yoohyojun <80876813+yoohyojun@users.noreply.github.com>
2023-12-28 16:56:21 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -1,21 +1,10 @@
 def solution(cards1, cards2, goal):
-    # idx1, idx2 를 따로 관리한다.
-    index1 = 0
-    index2 = 0
-
     for word in goal:
-        # 초기화
-        is_find = False
-
-        if len(cards1) > index1 and word == cards1[index1]:
-            is_find = True
-            index1 += 1
-
-        if not is_find:
-            if len(cards2) > index2 and word == cards2[index2]:
-                is_find = True
-                index2 += 1
-
-        if not is_find:
-            return 'No'
+        if len(cards1) > 0 and word == cards1[0]:
+            cards1.pop(0)
+            continue
+        if len(cards2) > 0 and word == cards2[0]:
+            cards2.pop(0)
+            continue
+        return 'No'
     return 'Yes'
