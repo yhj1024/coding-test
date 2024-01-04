95f46714985643b05abd3e5a78faf78d3e7b7ffd
yoohyojun <80876813+yoohyojun@users.noreply.github.com>
2024-01-04 12:21:11 +0900

    [Silver V] Title: 나이순 정렬, Time: 1648 ms, Memory: 62968 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+
+public class Main {
+    private static class Member {
+        int age;
+        String name;
+        int index;
+        public Member(int age, String name, int index) {
+            this.age = age;
+            this.name = name;
+            this.index = index;
+        }
+        public void print() {
+            System.out.println(age + " " + name);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
+        Member[] members = new Member[N];
+        for(int i=0; i<N; i++) {
+            String[] lines = bf.readLine().split(" ");
+            members[i] = new Member(Integer.parseInt(lines[0]), lines[1], i);
+        }
+        Arrays.sort(members, (member1, member2) -> {
+            if(member1.age == member2.age) {
+                return member1.index - member2.index;
+            } else {
+                return member1.age - member2.age;
+            }
+        });
+        Arrays.stream(members).forEach(member -> member.print());
+    }
+
+
+}
