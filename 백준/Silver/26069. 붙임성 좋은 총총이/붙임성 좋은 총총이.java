8f90c31eabea17e70411c6b2aca0081427a6edc3
yoohyojun <80876813+yoohyojun@users.noreply.github.com>
2024-01-10 15:56:05 +0900

    [Silver IV] Title: 붙임성 좋은 총총이, Time: 156 ms, Memory: 14800 KB -BaekjoonHub

---
+++ 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Set<String> dancers = new HashSet<>();
+        dancers.add("ChongChong");
+        int result = 1;
+        int N = Integer.parseInt(br.readLine());
+        for(int i=0; i<N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            String a = st.nextToken();
+            String b = st.nextToken();
+            if(dancers.contains(a) && !dancers.contains(b)) {
+                dancers.add(b);
+                result += 1;
+            }
+            if(dancers.contains(b) && !dancers.contains(a)) {
+                dancers.add(a);
+                result += 1;
+            }
+        }
+        br.close();
+        System.out.println(result);
+    }
+
+    // br
+    // bw
+    // int N 기록 수 입력 받음
+    // Set<String> Dances 춤추는 명단 (ChongChong) 초기화
+    //  for n
+    //   st 입력받음
+    //   string a = st.nexttoken
+    //   String b = st.nexttoken
+    //   a가 set에 있는지 확인
+    //   있으면 b도 set에 추가함
+    //  set 사이즈 출력
+
+}
