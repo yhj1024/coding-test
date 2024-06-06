3db9d690fe2d3d54deaf57800877d46955e830d9
yoohyojun <80876813+yoohyojun@users.noreply.github.com>
2024-06-06 20:35:20 +0900

    [Silver IV] Title: 기타줄, Time: 128 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Silver/1049. 기타줄/기타줄.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        // 패키지 판매 최소 가격
+        // 낱개 판매 최소 가격
+        // 구매할 기타줄 개수
+        // 구매금액
+
+        // 낱개 금액 * 6이 패키지 구매 금액보다 높은 경우 무조건 낱개 구매하는게 낫다
+        
+        // while 구매할 기타줄 개수 > 0
+        //  if 패키지 판매 최소 가격 > 낱개 판매 최소가*구매할 기타줄 개수
+        //   구매할 기타줄 개수 = 구매할 기타줄 개수 - 6
+        //   구매금액 = 구매금액 + 패키지 판매 최소 가격
+        //  else
+        //   구매할 기타줄 개수 = 0
+        //   구매금액 = 낱개 판매 최소가*구매할 기타줄 개수
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] strs = br.readLine().split(" ");
+        int N = Integer.parseInt(strs[0]);
+        int M = Integer.parseInt(strs[1]);
+        int minPackagePrice = Integer.MAX_VALUE;
+        int minSinglePrice = Integer.MAX_VALUE;
+        int purchaseAmount = 0;
+
+        for(int i=0; i<M; i++) {
+            String[] lines = br.readLine().split(" ");
+            int packagePrice = Integer.parseInt(lines[0]);
+            int singlePrice = Integer.parseInt(lines[1]);
+            if(minPackagePrice > packagePrice) {
+                minPackagePrice = packagePrice;
+            }
+            if(minSinglePrice > singlePrice) {
+                minSinglePrice = singlePrice;
+            }
+        }
+            
+        // 낱개 금액 * 6이 패키지 구매 금액보다 높은 경우 무조건 낱개 구매하는게 낫다
+        if(minSinglePrice * 6 <= minPackagePrice) {
+            minPackagePrice = Integer.MAX_VALUE;
+        }
+
+        while(N > 0) {
+            if(minPackagePrice < N * minSinglePrice) {
+                purchaseAmount += minPackagePrice;
+                N -= 6;
+            } else {
+                purchaseAmount += N * minSinglePrice;
+                N = 0;
+            }
+        }
+        System.out.println(purchaseAmount);
+    }
+}
