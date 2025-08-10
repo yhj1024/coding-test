40ae120ce937edac8934c5e3a330bd6d74a8e5ae
HJ <80876813+yhj1024@users.noreply.github.com>
2025-08-10 12:54:02 +0900

    [level 2] Title: 짝지어 제거하기, Time: 193.27 ms, Memory: 37.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,15 @@
+function solution(s) { 
+    const stack = [];
+    for(let i=0; i<s.length; i++) {
+        if(i === 0) {
+            stack.push(s[i]);
+            continue;
+        }
+        if(stack[stack.length - 1] === s[i]) {
+            stack.pop();
+        } else {
+            stack.push(s[i])
+        }
+    }
+    return stack.length ? 0 : 1;
+}
