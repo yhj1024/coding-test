836343b9387992b2b09d7c78dfd078f6f477789f
HJ <80876813+yhj1024@users.noreply.github.com>
2025-08-10 22:34:25 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.29 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,30 +1,16 @@
 function solution(n, words) {
     const set = new Set();
-    const gamers = [];
-    for(let i=0; i<words.length; i++) {
+    const gamers = [1];
+    set.add(words[0])
+    for(let i=1; i<words.length; i++) {
         const nowGamer = i % n;
-        if(i === 0) {
-            set.add(words[i])
-            gamers[nowGamer] ? gamers[nowGamer] += 1 : gamers[nowGamer] = 1;
-            continue;
-        }
         const beforeLastWord = words[i-1][words[i-1].length - 1];
         const nowLastWord = words[i][0];
-        if((i>0 && beforeLastWord !== nowLastWord)) {
-            return [nowGamer + 1, (gamers[nowGamer] ?? 0) + 1]            
-        }
-        if(set.has(words[i])) {
+        if(set.has(words[i]) || beforeLastWord !== nowLastWord) {
             return [nowGamer + 1, (gamers[nowGamer] ?? 0) + 1]
         }
         set.add(words[i])
         gamers[nowGamer] ? gamers[nowGamer] += 1 : gamers[nowGamer] = 1;
     }
-    
-    // 단어 Set
-    // 현재 순번당 진행 숫자 배열
-    // 단어들을 반복 시작
-    // 단어 Set에 현재 단어가 있는 경우
-    // 리턴 [해당 플레이어 + 해당 플레이어의 배열 + 1]
-
     return [0, 0];
 }
