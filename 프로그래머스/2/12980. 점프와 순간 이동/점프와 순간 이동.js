e281a0cf54328a306eaac9bd7e4f886bfccad790
HJ <80876813+yhj1024@users.noreply.github.com>
2025-08-16 17:16:37 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -1,15 +1,9 @@
-function solution(n) {
-    // 뒤에서 부터 출발
-    // 반씩 계속 나눔 홀수인 경우 -1 하고 체력 1 사용
-    let result = 0;
-    while(n > 0) {
-        if(n % 2 === 0) {
-            n /= 2
-        } else {
-            n -= 1;
-            result += 1;
-        }
-    }
-    return result
-}
+function solution(n)
+{
+    var ans = 0;
 
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    console.log('Hello Javascript')
+
+    return ans;
+}
